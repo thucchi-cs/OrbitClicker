1ed20feb4e7bb2d76b325d0e709c1521ec235cdd
Thuc Chi Do <thucchi.code@gmail.com>
2026-07-09 14:45:52 -0500

    spinning needle

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello")
+const needle = document.getElementById("needle");
+let speed = 2;
+let angle = 0;
+
+const rotate = () => {
+    angle += speed
+    needle.style.transform = `rotate(${angle}deg)`;
+}
+
+setInterval(() => {
+    rotate()
+}, 40)
